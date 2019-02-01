103beb74b4b08883e2b9494afd3dd35dae3b39d6
Mijka <38231801+mijkami@users.noreply.github.com>
2019-02-01 10:49:42 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
 
-10.times do
-    city = City.create(name: Faker::Address.city)
+9.times do |index|
+	u = Doctor.create
+	u.first_name = "Prenom" + index.to_s
+        u.last_name = "Nom" + index.to_s
+	u.speciality = "SPECIALITY" + index.to_s 
+        u.postal_code = "0100" + index.to_s
+	u.save
 end
 
-10.times do
-    dogsitter = Dogsitter.create(name: Faker::Artist.name, city_id: City.ids.sample)
-    end
-
-10.times do
-    dog = Dog.create(name: Faker::Creature::Dog.name, city_id: City.ids.sample)
+9.times do |index|
+	u = Appointment.all.create
+	u.date = Date.today
+	u.save
 end
 
-10.times do
-    stroll = Stroll.create(date: Faker::Date.forward(100), city_id: City.ids.sample, dogsitter_id: Dogsitter.ids.sample)
-end
-
-10.times do
-    joindogstroll = JoinDogStroll.create(dog_id: Dog.ids.sample, stroll_id: Stroll.ids.sample)
+9.times do |index|
+	u = Patient.create
+	u.first_name = "Prénom" + index.to_s
+        u.last_name = "Nom" + index.to_s
+	u.save
 end
